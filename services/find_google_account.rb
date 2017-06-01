f66fd0ec6a5506fe398cccc76be60ddd1b18b46c
LeoLee <leo.li@iss.nthu.edu.tw>
2017-06-02 02:44:32 +0800

    ADD for sso

---
+++ services/find_google_account.rb
@@ -0,0 +1,31 @@
+require 'http'
+
+# Returns an authenticated user, or nil
+class FindAuthenticatedGoogleAccount
+  def initialize(config)
+    @config = config
+  end
+
+  def call(code)
+    access_token = get_access_token(code)
+    get_sso_account_from_api(access_token)
+  end
+
+  def get_access_token(code)
+    HTTP.headers(accept: 'application/json')
+        .post('https://www.googleapis.com/oauth2/v4/token',
+              form: { client_id: @config.GH_CLIENT_ID,
+                      client_secret: @config.GH_CLIENT_SECRET,
+                      grant_type: 'authorization_code',
+                      redirect_uri: "#{@config.APP_URL}/google_callback",
+                      code: code })
+        .parse['access_token']
+  end
+
+  def get_sso_account_from_api(access_token)
+    response = HTTP.headers(accept: 'application/json')
+                   .get("#{@config.API_URL}/google_account?access_token=#{access_token}")
+    puts "SSO: #{response.parse}"
+    response.code == 200 ? response.parse : nil
+  end
+end
